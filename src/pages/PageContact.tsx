a3c9335c840338e60ba73853a4efdbc77ee26527
Your Name <you@example.com>
2022-12-03 01:51:12 +0100

    product paragraph height added

--- src/pages/PageContact.tsx
+++ src/pages/PageContact.tsx
@@ -1,11 +1,29 @@
+import "../styles/pages/pageContact.scss";
+
 export const PageContact = () => {
   return (
-    <form>
-      <fieldset>
-        <legend>New Product</legend>
-        <input type="text" />
-        <input type="text" />
-      </fieldset>
-    </form>
+    <div className="pageContact">
+      <form>
+        <fieldset>
+          <legend>
+            <span>type of requests</span>
+          </legend>
+
+          <div>
+            <div>
+              <label>type of requests *</label>
+            </div>
+            <select>
+              <option value="">- Choose -</option>
+              <option value="">Beauty/Product Advice</option>
+              <option value="">Voucher/trial request</option>
+              <option value="">Inquiry about ingredients</option>
+              <option value="">Product-Feedback</option>
+              <option value="">Website-Problem</option>
+            </select>
+          </div>
+        </fieldset>
+      </form>
+    </div>
   );
 };
